b5147b64255c0b218009ce683df54dee0cb7342c
owamsley <Mickey20122012>
2019-03-08 17:39:30 -0700

    All micro bit tutorials and code

---
+++ microTut2.py
@@ -0,0 +1,10 @@
+from microbit import *
+
+sailboat = Image("00530:"
+             "00533:"
+             "00530:"
+             "99999:"
+             "09990")
+list = [Image.GHOST, "boo"]
+
+display.show(list)
---
+++ microTut5.py
@@ -0,0 +1,3 @@
+import music
+
+music.play("c4:4")
